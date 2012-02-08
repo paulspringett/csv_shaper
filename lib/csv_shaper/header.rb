b49892aa69b58a2b95f33ce04bbaf6d06e03c0ee
Paul Springett <paul@springett.me>
2012-02-08 20:27:32 +0000

    Initial Header class

---
+++ lib/csv_shaper/header.rb
@@ -0,0 +1,42 @@
+module CsvShaper
+  class Header < BlankSlate
+
+    attr_reader :klass, :columns, :mappings, :mapped_columns
+
+    def self.build(*args)      
+      header = new(*args)
+      
+      if block_given?
+        header.tap { |header| yield header }
+      elsif header.klass && header.columns.empty?
+        header.columns(header.klass.attribute_names)
+      end
+      
+      header
+    end
+    
+    def initialize(klass, columns = [])
+      @klass = klass
+      @columns = columns
+      @mappings = {}
+    end
+    
+    def mappings(hash = {}) 
+      @mappings.merge!(hash)
+      puts "mappings: #{@mappings.inspect}"
+    end
+    
+    def columns(*args)
+      array = []
+      args.each { |i| array.push(i) }
+      @columns = array.map(&:to_sym)
+    end
+    
+    def mapped_columns
+      @columns.map do |column|
+        @mappings[column] || column.to_s.humanize
+      end
+    end
+
+  end
+end
