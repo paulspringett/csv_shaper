79a2a8c4cea562489b529084b31efad224ca4616
Paul Springett <paul@springett.me>
2012-07-20 23:04:50 +0100

    Expand sample file

--- test/sample.rb
+++ test/sample.rb
@@ -11,16 +11,23 @@ users = [user]
 shaper = CsvShaper::Shaper.encode do |csv|
 
   csv.headers do |csv|
-    csv.columns :name, :gender, :age, :foo, :bar
+    csv.columns :name, :gender, :age
     csv.mappings name: "Full name", gender: "Sex"
   end
   
+  # csv.headers User
+  # csv.headers :name, :gender, :age
+
   csv.rows users do |csv, user|
     csv.cells :name, :age
     csv.cell :gender
-    # csv.cells :foo, :bar
+  end
+
+  csv.row do |csv|
+  	csv.cell :age, 120 # total age
   end
 
 end
 
 puts shaper.inspect
+puts shaper.header.inspect
