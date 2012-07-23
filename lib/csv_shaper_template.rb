dc6bc6fb68b6e4c5878ddd19235297408ede8904
Paul Springett <paul@springett.me>
2012-07-23 18:03:36 +0100

    Force download of CSV and customise filename

--- lib/csv_shaper_template.rb
+++ lib/csv_shaper_template.rb
@@ -2,7 +2,7 @@ class CsvShaperTemplate < CsvShaper::Shaper
   def self.encode(context)
     new(context).tap { |shaper| yield shaper }.to_csv
   end
-  
+
   def initialize(context)
     @context = context
     super()
@@ -15,6 +15,12 @@ class CsvShaperHandler
 
   def self.call(template)
     %{
+      unless defined?(ActionMailer) && defined?(ActionMailer::Base) && controller.is_a?(ActionMailer::Base)
+        @filename ||= "\#{controller.action_name}.csv"
+        controller.response.headers["Content-Type"] ||= 'text/csv'
+        controller.response.headers['Content-Disposition'] = "attachment; filename='\#{@filename}'"
+      end
+
       CsvShaperTemplate.encode(self) do |csv|
         #{template.source}
       end
